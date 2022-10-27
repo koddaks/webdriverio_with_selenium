a48fd88d5bcd376b2707eae328539f2f7a7a5c6d
Borysov Kostyantyn <koss199710@gmail.com>
2022-10-27 13:20:54 +0300

    Run Docker Compose + in Github Action

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,5 +1,6 @@
 const LoginPage = require('../pageobjects/login.page');
 const SecurePage = require('../pageobjects/secure.page');
+// https://gist.github.com/cecilemuller/437d7340b9f095cf5635dc9780a05092  - Run Docker Compose + in Github Action
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
